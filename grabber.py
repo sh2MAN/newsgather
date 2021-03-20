5962b09e900644159933c18c4f364123fa90e473
Sergei Simonov <simons2007@yandex.ru>
2021-03-20 19:52:32 +0300

    Add grabber.py

---
+++ grabber.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime as dt
+import feedparser
+
+
+class Grabber:
+    """Получение последних новостей из запрошенного RSS канала"""
+
+    def news(self, limit):
+        """
+        Получение новостей из RSS канала
+            limit: количество последних новостей
+        """
+        news = []
+        url = self.urls[0]
+        fp = feedparser.parse(url)
+        all_feed = fp["items"]
+        
+        for feed in all_feed:
+            if limit > 0:
+                piece_news= {}
+                pub_date = dt.strptime(
+                    feed['published'], '%a, %d %b %Y %H:%M:%S %z'
+                )
+                piece_news['title'] = feed['title']
+                piece_news['link'] = feed['link']
+                piece_news['desc'] = feed['description']
+                piece_news['pub_date'] = pub_date.strftime('%d.%m.%Y %H:%M')
+                news.append(piece_news)
+                
+            limit -= 1
+        return news
+    
+    def grub(self, link):
+        """
+        Получаем данные статьи по ссылке
+        """
+        raise NotImplemented
+
+    
+class Lenta(Grabber):
+    def __init__(self):
+        self.urls = [
+            'http://lenta.ru/rss'
+        ] 
+        self.tmp_news = []
+
+        
+    def grub(self, link) -> dict:        
+        article_dict = {}
+        r = requests.get(link).text
+        soup = BeautifulSoup(r, 'html.parser')
+        article = soup.find('div', class_='b-topic__content')
+        article_dict['title'] = article.find('h1', class_='b-topic__title').text.replace('\xa0',' ')
+
+        img = article.find('div', class_='b-topic__title-image').find('img', src=True)
+        article_dict['image'] = img.get('src') if img is not None else None
+
+        contents = article.find('div', itemprop='articleBody')
+        for content in contents.find_all('p'):
+            article_dict['content'] = article_dict.get('content', []) + [content.text]
+
+        return article_dict
+        
+
+
+if __name__ == '__main__':
+    grabber = Lenta()
+    news = grabber.news(limit=3)    
+    data = [grabber.grub(news[i]['link']) for i in range(len(news))]
+    
+    print(data)
