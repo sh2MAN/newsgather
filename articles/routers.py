ee7b715fca1d6ae81c249bc492363e3eab6259da
Sergei Simonov <simons2007@yandex.ru>
2021-03-23 23:03:59 +0300

    Update GET request /news

--- articles/routers.py
+++ articles/routers.py
@@ -1,18 +1,32 @@
 from typing import List
 
 from core import utils
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
+from grabber import Grabber
 from sqlalchemy.orm import Session
 
-from .crud import create_news, get_allnews
+from .crud import create_news, get_all_news, get_news_by_link
 from .schemas import NewsCreate, NewsList
 
 router = APIRouter()
 
 
 @router.get('/news', response_model=List[NewsList])
-def news_list(db: Session = Depends(utils.get_db)):
-    return get_allnews(db)
+def news_list(
+    db: Session = Depends(utils.get_db),
+    limit: int = Query(None, gt=0, description='Количество новостей')
+):
+    grabber = Grabber()
+    data = grabber.lenta.news(limit)
+
+    for event in data:
+        news = get_news_by_link(db, event.get('link'))
+        if news is None:
+            news = grabber.lenta.grub(event.get('link'))
+            news = NewsCreate(**news, pub_date=event.get('published'))
+            create_news(db, news)
+
+    return get_all_news(db, limit)
 
 
 @router.post('/news', response_model=NewsCreate)
